ae2e64b4c2953923fe224765a3ff47f4f6522b7d
Pream Totaram <pream@preamtotaram.com>
2018-06-09 20:15:08 -0400

    get javascript working

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,6 @@
 (function($) {
     "use strict";
-    $(document).ready(function() {
-        $.colorbox({
-            href: 'https://modal-covenanthouse.pantheonsite.io/modal/content',
-            width: "200px",
-            height: "200px"
-        });
+    $(document).ready(function () {
+    //    AJAX REQUEST GOES HERE
     }).load();
 })(jQuery);
