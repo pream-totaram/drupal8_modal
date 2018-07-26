5a9d102913d6ebb346e658e49fac23f9c112a6ca
Pream Totaram <pream@preamtotaram.com>
2018-07-25 23:18:52 -0400

    comment until needed

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@
     "use strict";
     $(document).ready(function () {
         $.ajax("http://localhost:8088/modal/content").then(function(res) {
-            $(res).appendTo('body').modal();
+            // $(res).appendTo('body').modal();
         });
     }).load();
 })(jQuery);
